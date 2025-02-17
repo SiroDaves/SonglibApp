f56098904fce9500e39d93a0be7baf7681e8ec65
Siro Daves <sirodevske@gmail.com>
2025-02-17 22:22:02 +0300

    marquee fixes

--- components/ui/book-card.tsx
+++ components/ui/book-card.tsx
@@ -7,19 +7,18 @@ interface BookCardProps {
 
 const BookCard: React.FC<BookCardProps> = ({ book }) => {
   return (
-    <div className="w-40 min-w-fit h-fit flex flex-col items-center justify-center transition-all duration-500 m-3 sm:m-5 rounded-lg group relative hover:scale-[1.15] cursor-pointer"
-      key={book.bookId}>
-      <div className="h-full w-full rounded-lg border border-[#BF360C] shadow-none shadow-gray-50 group-hover:border-red-500 transition-all duration-500">
-        <div className="flex -translate-y-[1px] justify-center">
-          <div className="w-3/4">
-            <div className="h-[1px] w-full bg-gradient-to-r from-transparent via-red-500 to-transparent" />
-          </div>
-        </div>
-        <div className="flex flex-col items-center justify-center gap-3 p-6">
-          <div className="h-8 sm:h-10">
-            <FaBookOpen size={36} color="#331900" />
-          </div>
-          <p className="text-primary text-sm sm:text-lg"> {book.title} </p>
+    <div
+      className="w-32 h-32 flex flex-col items-center justify-center transition-transform duration-500 m-3 sm:m-5 rounded-full group relative hover:scale-110 cursor-pointer"
+      key={book.bookId}
+    >
+      <div className="w-full h-full flex flex-col items-center justify-center rounded-full border border-[#BF360C] p-4 bg-white shadow-lg">
+        <div className="flex flex-col items-center justify-center gap-1 text-center">
+          <h4 className="text-primary text-md font-semibold">
+            {book.title}
+          </h4>
+          <p className="text-primary text-xs">
+            {book.songs} {book.subTitle}<br/>songs
+          </p>
         </div>
       </div>
     </div>
